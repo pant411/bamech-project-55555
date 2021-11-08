178e68235b7328523b0038fea0347c7f12ff3e9a
pant411 <pantchanit2561@hotmail.com>
2021-11-08 16:46:23 +0700

    test1

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1636364730,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/pngtreearchitecturestonetexturebrickwallpngimage_-sheet0.png",
+		"images/ดาวน์โหลด-sheet0.png",
+		"images/obj-sheet0.png",
+		"images/s_obj-sheet0.png",
+		"images/s2_obj-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
